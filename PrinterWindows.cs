8109729ac70fc2eae83df8465cb15dc8952e5e7b
Asripramudiya <asri.pr@students.amikom.ac.id>
2020-05-06 23:24:21 +0700

    inisialisasi project

---
+++ PrinterWindows.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TugasPolymorphism
+{
+    public class PrinterWindows
+    {
+        public string Merk { get; set; }
+
+        public virtual void Show()
+        {
+            Console.WriteLine("\n");
+        }
+        
+        public virtual void Print()
+        {
+            Console.WriteLine("\n");
+        }
+    }
+}
